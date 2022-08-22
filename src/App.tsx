66a3625fbaa89a71b92ea6afda5f2eede7a3cdda
Shuhari <japaishikawa@gmail.com>
2022-08-22 13:54:44 -0300

    App

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
